61a779349f74c1ffec093f29891bd68dacfdea23
Chis Ioan <chisulescu@gmail.com>
2020-06-23 01:38:01 +0300

    -- add post methods for books (create, update, delete)

--- inventory_server/src/books/api/urls.py
+++ inventory_server/src/books/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('', BookListView.as_view()),
+    path('create', BookCreateView.as_view()),
+    path('<pk>/update', BookUpdateView.as_view()),
+    path('<pk>/delete', BookDeleteView.as_view()),
     path('<pk>', BookDetailView.as_view())
 ]
--- inventory_server/src/books/api/views.py
+++ inventory_server/src/books/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from books.models import Book
 from .serializers import BookSerializer
 
@@ -7,5 +7,17 @@ class BookListView(ListAPIView):
     serializer_class = BookSerializer
 
 class BookDetailView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookCreateView(CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDeleteView(DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
