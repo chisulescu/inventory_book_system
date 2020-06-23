4b33f90ec74e993467e0227dcc27397df896d01f
Chis Ioan <chisulescu@gmail.com>
2020-06-23 17:02:47 +0300

    -- add login page

--- inventory_frontend/main.js
+++ inventory_frontend/main.js
@@ -1,5 +1,3 @@
-import fetch from 'electron-fetch'
-
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
@@ -12,14 +10,13 @@ let mainWindow;
 app.on('ready', function(){
   // Create new window
   mainWindow = new BrowserWindow({});
-  fetch('http://127.0.0.1:8000/books/')
-    .then(res => res.text())
-    .then(body => console.log(body))
   // Load html in window
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'mainWindow.html'),
     protocol: 'file:',
     slashes:true
   }));
+
+
 });
 
